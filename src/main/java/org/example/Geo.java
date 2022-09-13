4a5c92f68148021976539b598514facd3488ffb0
johnsmith <iea2001@mail.ru>
2022-09-13 20:53:21 +0700

    Request and write in list json objects

--- src/main/java/org/example/Geo.java
+++ src/main/java/org/example/Geo.java
@@ -1,2 +1,38 @@
-package org.example;public class Geo {
+package org.example;
+
+public class Geo {
+    private String lat;
+    private String lng;
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public Geo() {
+    }
+
+    public Geo(String lat, String lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    @Override
+    public String toString() {
+        return "Geo{" +
+                "lat='" + lat + '\'' +
+                ", lng='" + lng + '\'' +
+                '}';
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,36 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+    final static String API_URL = "https://jsonplaceholder.typicode.com/users";
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(API_URL)).build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        ObjectMapper mapper = new ObjectMapper();
+        List<Users> users = mapper.readValue(response.get().body(), new TypeReference<List<Users>>() {
+        });
+        users.forEach(System.out::println);
+
+        //ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
+        //writer.writeValue(new File("PATH"), users);
     }
 }
